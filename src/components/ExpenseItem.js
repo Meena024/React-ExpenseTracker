c1fad08481692fe087545a0bedcb7b91a33c2d1b
Meena024 <maill2rmeena@gmail.com>
2024-02-03 16:34:50 +0530

    ExpenseItem

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+function ExpenseItem() {
+    return (
+      <div>
+        <h2>Expense Item!</h2>
+        <p>
+          <li>Food Rs.10</li>
+          <li>Petrol Rs.100</li>
+          <li>Movies Rs.200</li>
+        </p>
+        <p>
+          <li>Food Rs.20</li>
+          <li>Petrol Rs.200</li>
+          <li>Movies Rs.300</li>
+        </p>
+      </div>
+    );
+  }
+  
+  export default ExpenseItem;
+  
