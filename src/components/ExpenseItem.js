a3dfb878da2bf9ff9fa284f4bcd51e91e11454d3
Meena024 <maill2rmeena@gmail.com>
2024-02-04 15:57:14 +0530

    ExpenseItem with dynamic data display

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,18 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2024, 0, 4);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 424.12;
+    const expenseLocation = 'chennai';
+
     return (
       <div className='expense-item'>
-        <div>January 4th 2023</div>
+        <div>{expenseDate.toDateString()}</div>
         <div  className='expense-item__description'>
-          <h2>Car Insurance</h2>
-          <div  className='expense-item__price'>$500</div>
+          <h2>{expenseTitle}</h2>
+          <h2>{expenseLocation}</h2>
+          <div  className='expense-item__price'>${expenseAmount}</div>
         </div>
       </div>
     );
