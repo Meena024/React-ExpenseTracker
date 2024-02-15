f48ce5854295c54612436feb996529f2df22ce27
Meena024 <maill2rmeena@gmail.com>
2024-02-15 10:46:12 +0530

    Onsubmit log the data using object and empty the input fields

--- src/components/New Expense/ExpenseForm.js
+++ src/components/New Expense/ExpenseForm.js
@@ -16,12 +16,25 @@ const ExpenseForm = () => {
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
   };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const expenseData = {
+      title: eneteredTitle,
+      amount: eneteredAmount,
+      date: new Date(eneteredDate)
+    }
+    console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  }
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={eneteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -29,6 +42,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={eneteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -38,6 +52,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={eneteredDate}
             onChange={dateChangeHandler}
           />
         </div>
