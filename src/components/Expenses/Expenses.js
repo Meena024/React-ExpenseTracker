d56475e35a99021aeecee9c9dcfc0cbdc6048036
Meena024 <maill2rmeena@gmail.com>
2024-02-17 23:11:01 +0530

    Expenses list disintegrated from Expenses.js. Added conditional returns.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpenseFilter from "./ExpenseFilter";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("all");
@@ -10,29 +10,20 @@ function Expenses(props) {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-  const filteredExpenses = (filteredYear=='all')?(props.expenseList):(props.expenseList.filter(expense => {
-    return expense.date.getFullYear().toString()===filteredYear;
-  }))
+  const filteredExpenses =
+    filteredYear === "all"
+      ? props.expenseList
+      : props.expenseList.filter((expense) => {
+          return expense.date.getFullYear().toString() === filteredYear;
+        });
 
-  let expensesContent = <p style={{color: 'white'}}>No expenses found!</p>;
-  if(filteredExpenses.length>0){
-    expensesContent = filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          location={expense.location}
-        />
-      ))
-  }
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       ></ExpenseFilter>
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 }
