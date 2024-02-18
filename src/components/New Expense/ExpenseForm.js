4f843b363896e6cb5681d61eed65429cdd758217
Meena024 <maill2rmeena@gmail.com>
2024-02-18 16:47:41 +0530

    Add new expense button added and form hidden

--- src/components/New Expense/ExpenseForm.js
+++ src/components/New Expense/ExpenseForm.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
   const [eneteredTitle, setEnteredTitle] = useState("");
   const [eneteredAmount, setEnteredAmount] = useState("");
   const [eneteredDate, setEnteredDate] = useState("");
+  const [isFormVisible, setIsFormVisible] = useState(false);
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -29,42 +29,61 @@ const ExpenseForm = (props) => {
     setEnteredAmount("");
     setEnteredDate("");
   };
+
+  const cancelHandler = () => {
+    setIsFormVisible(false);
+  };
+
+  const showFormHandler = () => {
+    setIsFormVisible(true);
+  };
+
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={eneteredTitle}
-            onChange={titleChangeHandler}
-          />
+    <div>
+      {isFormVisible && (
+        <form onSubmit={submitHandler}>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input
+                type="text"
+                value={eneteredTitle}
+                onChange={titleChangeHandler}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Amount</label>
+              <input
+                type="number"
+                min="0.01"
+                step="0.01"
+                value={eneteredAmount}
+                onChange={amountChangeHandler}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Date</label>
+              <input
+                type="date"
+                min="2019-01-01"
+                max="2022-12-31"
+                value={eneteredDate}
+                onChange={dateChangeHandler}
+              />
+            </div>
+          </div>
+          <div className="new-expense__actions">
+            <button onClick={cancelHandler}>Cancel</button>
+            <button type="submit">Add Expense</button>
+          </div>
+        </form>
+      )}
+      {!isFormVisible && (
+        <div className="new-expense__actions">
+          <button onClick={showFormHandler}>Add New Expense</button>
         </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={eneteredAmount}
-            onChange={amountChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2022-12-31"
-            value={eneteredDate}
-            onChange={dateChangeHandler}
-          />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
+      )}
+    </div>
   );
 };
 
