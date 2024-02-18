8ef96c9d0e737cf713291a9f0162b42d3b29d2e6
Meena024 <maill2rmeena@gmail.com>
2024-02-18 15:49:13 +0530

    Added 'Only single expense here. Please add more'

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -5,6 +5,7 @@ const ExpensesList = (props) => {
   if (props.items.length === 0) {
     return <h2 className="expenses-list__fallback">Found no expenses!</h2>;
   }
+  let singleLength=(props.items.length===1)?(<h2 className="expenses-list__fallback">Only single Expense here. Please add more...</h2>):'';
   return (
     <ul className="expenses-list">
       {props.items.map((expense) => (
@@ -16,6 +17,7 @@ const ExpensesList = (props) => {
           location={expense.location}
         />
       ))}
+      {singleLength}
     </ul>
   );
 };
